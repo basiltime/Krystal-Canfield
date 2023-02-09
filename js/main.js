a5075bf2836d4a8ca6f22c386967ebcab08a3fe7
basiltime <krystalcanfield04@gmail.com>
2023-02-09 17:04:33 -0500

    Add recent projects. Change form to use axios. Fix some styling and copy

---
+++ js/main.js
@@ -0,0 +1,37 @@
+/* Hamburger menu */
+const navDropdown = document.getElementById("navDropdown");
+let toggleMenu = () => {
+    if (!navDropdown.classList.contains('show-menu')) {
+        navDropdown.classList.add('show-menu');
+    } else {
+        navDropdown.classList.remove('show-menu');
+    }
+}
+const burger = document.getElementById("burger");
+burger.addEventListener("click", toggleMenu);
+/* End hamburger menu */
+
+/* Form Submit */
+const form = document.getElementById('contactForm')
+const name_input = document.getElementById('name');
+const message_input = document.getElementById('message');
+const email_input = document.getElementById('email');
+
+form.addEventListener('submit', formSubmit);
+
+function formSubmit(event) {
+   event.preventDefault();
+   console.log('hit')
+   axios.defaults.headers.post['Content-Type'] = 'application/json';
+    axios.post('https://formsubmit.co/dfb7451573ee6e900f208ff4f6ee2c75', {
+        name: name_input,
+        message: message_input,
+        email: email_input,
+        _subject: "New contact from personal website",
+        _next: "https://basiltime.github.io/personal_site/", // change when deployed live!
+        _autoresponse: "Thank you for reaching out! I'm excited to discuss your project with you. I'll respond to your email as soon as I can."
+    })
+    .then(response => console.log(response))
+    .catch(error => console.log(error));
+}
+/* End Form Submit */
